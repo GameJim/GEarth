02baba220d484b9dfc761500c436fe79c59126f2
景志敏(10038012) <hbxsjzm@163.com>
2022-01-14 18:07:18 +0800

    增添RenderSystem模块

---
+++ GEarth/include/RenderSystem/renderable.h
@@ -0,0 +1,29 @@
+﻿/*!
+*@File    renderable.h
+*@Brief   渲染数据类
+*@Date    2022/1/14
+*/
+#pragma once
+#include "common/apiDescription.h"
+namespace render
+{
+    //vertex, normals, texcoords
+
+    //纹理若绑定
+
+    //颜色不一定有
+
+  
+    class CORE_PUBLIC CRenderable
+    {
+    public:
+        CRenderable() = default;
+        virtual ~CRenderable() = default;
+
+        //顶点数组
+        //纹理数据必定与顶点数据1对1
+
+        //物体的材质
+       
+    };
+}
